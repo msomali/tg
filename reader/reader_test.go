ce1ffe5b9ab856706a566910c03a6bf0d04244f3
Pius Alfred <me.pius1102@gmail.com>
2021-06-23 16:57:02 +0300

    merge readers

--- reader/reader_test.go
+++ reader/reader_test.go
@@ -10,13 +10,12 @@ func TestMake(t *testing.T) {
 	type args struct {
 		options []Option
 	}
-	tests := []struct {
+	var tests []struct {
 		name string
 		args args
 		want Reader
-	}{
-		// TODO: Add test cases.
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Make(tt.args.options...); !reflect.DeepEqual(got, tt.want) {
