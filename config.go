74a9d7ede9d8da9b550c95a2110fabd0766b24ba
Pius Alfred <me.pius1102@gmail.com>
2021-07-01 19:36:33 +0300

    config command

--- config.go
+++ config.go
@@ -1,3 +1,58 @@
 package tg
 
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+)
+
+func (client *Client) MakeConfigCommand()[]*cli.Command{
+
+	var cms []*cli.Command
+
+	config := &cli.Command{
+		Name:                   "config",
+		Usage:                  "configure the tool",
+		UsageText:              "tg config --push --file=file.yaml ",
+		Description:            "run this command to configure the tools to use credentials provided by tigo pesa in integration stage",
+		Before: func(context *cli.Context) error {
+			fmt.Printf("checking if the configuration was done before")
+			return nil
+		},
+		After: func(context *cli.Context) error {
+			fmt.Printf("printing the config for assurance")
+			return nil
+		},
+		Action: func(context *cli.Context) error {
+			fmt.Printf("Write the config into config file")
+			return nil
+		},
+		OnUsageError: func(context *cli.Context, err error, isSubcommand bool) error {
+			return nil
+		},
+		Subcommands:            nil,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name: "file",
+				Value: "config.yaml",
+				Usage: "specify file with config",
+			},
+
+			&cli.BoolFlag{
+				Name:        "push",
+				Usage:       "configuration for push pay",
+				Value:       false,
+			},
+			&cli.BoolFlag{
+				Name:        "disburse",
+				Usage:       "configuration for disburse",
+				Value:       false,
+			},
+		},
+
+	}
+
+	cms = append(cms,config)
+
+	return cms
+}
 
--- tg.go
+++ tg.go
@@ -1,7 +1,6 @@
 package tg
 
 import (
-	"fmt"
 	"github.com/techcraftt/tigosdk/aw"
 	"github.com/techcraftt/tigosdk/pkg/tigo"
 	"github.com/techcraftt/tigosdk/push"
@@ -157,98 +156,6 @@ func app(client *Client)*cli.App{
 	return a
 }
 
-func makeCommands ()[]*cli.Command{
-
-	//tigo.BaseClient{
-	//	HttpClient: nil,
-	//	Ctx:        nil,
-	//	Timeout:    0,
-	//	Logger:     nil,
-	//	DebugMode:  false,
-	//}
-	//
-
-	//push.Config{
-	//	Username:              "",
-	//	Password:              "",
-	//	PasswordGrantType:     "",
-	//	ApiBaseURL:            "",
-	//	GetTokenURL:           "",
-	//	BillerMSISDN:          "",
-	//	BillerCode:            "",
-	//	PushPayURL:            "",
-	//	ReverseTransactionURL: "",
-	//	HealthCheckURL:        "",
-	//}
-
-	//
-	//&push.Client{
-	//	Config:          nil,
-	//	BaseClient:      nil,
-	//	CallbackHandler: nil,
-	//}
-	//
-
-	//aw.Config{
-	//	AccountName:   "",
-	//	AccountMSISDN: "",
-	//	BrandID:       "",
-	//	PIN:           "",
-	//	RequestURL:    "",
-	//}
-
-	//aw.Client{
-	//	Config:     nil,
-	//	BaseClient: nil,
-	//}
-	var cms []*cli.Command
-
-	config := &cli.Command{
-		Name:                   "config",
-		Usage:                  "configure the tool",
-		UsageText:              "tg config --push --file=file.yaml ",
-		Description:            "run this command to configure the tools to use credentials provided by tigo pesa in integration stage",
-		Before: func(context *cli.Context) error {
-			fmt.Printf("checking if the configuration was done before")
-			return nil
-		},
-		After: func(context *cli.Context) error {
-			fmt.Printf("printing the config for assurance")
-			return nil
-		},
-		Action: func(context *cli.Context) error {
-			fmt.Printf("Write the config into config file")
-			return nil
-		},
-		OnUsageError: func(context *cli.Context, err error, isSubcommand bool) error {
-			return nil
-		},
-		Subcommands:            nil,
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name: "file",
-				Value: "config.yaml",
-				Usage: "specify file with config",
-			},
-
-			&cli.BoolFlag{
-				Name:        "push",
-				Usage:       "configuration for push pay",
-				Value:       false,
-			},
-			&cli.BoolFlag{
-				Name:        "disburse",
-				Usage:       "configuration for disburse",
-				Value:       false,
-			},
-		},
-
-	}
-
-	cms = append(cms,config)
-
-	return cms
-}
 
 func (app *App) Run(args []string)error{
 	return app.cli.Run(args)
