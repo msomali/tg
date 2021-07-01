962ffbac9798373447e7ddf330ab512b457c0b14
Pius Alfred <me.pius1102@gmail.com>
2021-07-01 20:14:58 +0300

    config

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,20 @@ import (
 	"os"
 )
 
-
+var (
+	disburseAccountName   = "TIGO_DISBURSE_ACCOUNT_NAME"
+	disburseAccountMSISDN = "TIGO_DISBURSE_ACCOUNT_MSISDN"
+	disburseBrandID       = "TIGO_DISBURSE_BRAND_ID"
+	disbursePin           = "TIGO_DISBURSE_PIN"
+	disburseURL           = "TIGO_DISBURSE_URL"
+	pushUsername          = "TIGO_PUSH_USERNAME"
+	pushPassword          = "TIGO_PUSH_PASSWORD"
+	pushBillerMSISDN      = "TIGO_PUSH_BILLER_MSISDN"
+	pushBIllerCode        = "TIGO_PUSH_BILLER_CODE"
+	pushGetTokenURL       = "TIGO_PUSH_TOKEN_URL"
+	pushURL               = "TIGO_PUSH_URL"
+	pushCallbackURL       = "TIGO_PUSH_CALLBACK_URL"
+)
 
 func main() {
 	app := tg.Make(&tg.Config{})
