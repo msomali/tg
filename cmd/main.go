682bf35dafbefb6c9ff68def884f62e494e31d63
Pius Alfred <me.pius1102@gmail.com>
2021-07-10 14:02:52 +0300

    clean up

--- cmd/main.go
+++ cmd/main.go
@@ -6,68 +6,9 @@ import (
 	"os"
 )
 
-const (
-	envDisburseAccountName   = "TIGO_DISBURSE_ACCOUNT_NAME"
-	envDisburseAccountMSISDN = "TIGO_DISBURSE_ACCOUNT_MSISDN"
-	envDisburseBrandID       = "TIGO_DISBURSE_BRAND_ID"
-	envDisbursePin           = "TIGO_DISBURSE_PIN"
-	envDisburseURL           = "TIGO_DISBURSE_URL"
-	envPushUsername          = "TIGO_PUSH_USERNAME"
-	envPushPassword          = "TIGO_PUSH_PASSWORD"
-	envPushBillerMSISDN      = "TIGO_PUSH_BILLER_MSISDN"
-	envPushBIllerCode        = "TIGO_PUSH_BILLER_CODE"
-	envPushGetTokenURL       = "TIGO_PUSH_TOKEN_URL"
-	envPushURL               = "TIGO_PUSH_URL"
-	envPushCallbackURL       = "TIGO_PUSH_CALLBACK_URL"
-)
-
-var (
-	disburseAccountName   string
-	disburseAccountMSISDN string
-	disburseBrandID       string
-	disbursePin           string
-	disburseURL           string
-	pushUsername          string
-	pushBillerMSISDN      string
-	pushBIllerCode        string
-	pushGetTokenURL       string
-	pushURL               string
-	pushCallbackURL       string
-)
-
-func LoadConfingFromEnv() (*tg.Config, error) {
-	disburseAccountName = os.Getenv(envDisburseAccountName)
-
-	return &tg.Config{
-		ReferenceIDPrefix:         "TGCLI",
-		AppName:                   "tg",
-		AppVersion:                "v1.0.0",
-		ReleaseDate:               "July 2021",
-		MaxPushAmount:             1000000,
-		MinPushAmount:             1000,
-		MaxDisburseAmount:         1000000,
-		MinDisburseAmount:         1000,
-		DisburseAccountName:       disburseAccountName,
-		DisburseAccountMSISDN:     disburseAccountMSISDN,
-		DisburseBrandID:           disburseBrandID,
-		DisbursePIN:               disbursePin,
-		DisburseRequestURL:        disburseURL,
-		PushUsername:              pushUsername,
-		PushPassword:              pushUsername,
-		PushPasswordGrantType:     "password",
-		PushApiBaseURL:            "",
-		PushGetTokenURL:           "",
-		PushBillerMSISDN:          "",
-		PushBillerCode:            "",
-		PushPayURL:                "",
-		PushReverseTransactionURL: "",
-		PushHealthCheckURL:        "",
-	}, nil
-}
 
 func main() {
-
-	conf, err := LoadConfingFromEnv()
+	conf, err := tg.LoadConfFromEnv()
 	if err != nil {
 		log.Fatal(err)
 	}
--- config.go
+++ config.go
@@ -2,8 +2,101 @@ package tg
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/urfave/cli/v2"
+	"os"
+	"strconv"
 )
+
+const (
+	envDisburseAccountName   = "TIGO_DISBURSE_ACCOUNT_NAME"
+	envDisburseAccountMSISDN = "TIGO_DISBURSE_ACCOUNT_MSISDN"
+	envDisburseBrandID       = "TIGO_DISBURSE_BRAND_ID"
+	envDisbursePin           = "TIGO_DISBURSE_PIN"
+	envDisburseURL           = "TIGO_DISBURSE_URL"
+	envPushUsername          = "TIGO_PUSH_USERNAME"
+	envPushPassword          = "TIGO_PUSH_PASSWORD"
+	envPushBaseURL           = "TIGO_PUSH_BASE_URL"
+	envPushBillerMSISDN      = "TIGO_PUSH_BILLER_MSISDN"
+	envPushBillerCode        = "TIGO_PUSH_BILLER_CODE"
+	envPushGetTokenURL       = "TIGO_PUSH_TOKEN_URL"
+	envPushPayURL            = "TIGO_PUSH_PAY_URL"
+	envPushMaxAmount         = "TIGO_PUSH_MAX_AMOUNT"
+	envPushMinAmount         = "TIGO_PUSH_MIN_AMOUNT"
+	envDisburseMaxAmount     = "TIGO_DISBURSE_MAX_AMOUNT"
+	envDisburseMinAmount     = "TIGO_DISBURSE_MIN_AMOUNT"
+)
+
+func LoadConfFromEnv() (*Config, error) {
+	err := godotenv.Load("tigo.env")
+	if err != nil {
+		return nil, fmt.Errorf("error loading .env file %v\n", err)
+	}
+
+	pushMaxAmount := os.Getenv(envPushMaxAmount)
+	pushMinAmount:= os.Getenv(envPushMinAmount)
+	disburseMaxAmount := os.Getenv(envDisburseMaxAmount)
+	disburseMinAmount := os.Getenv(envDisburseMinAmount)
+
+	disburseName := os.Getenv(envDisburseAccountName)
+	disburseMSISDN := os.Getenv(envDisburseAccountMSISDN)
+	disburseBrandID := os.Getenv(envDisburseBrandID)
+	disbursePIN := os.Getenv(envDisbursePin)
+	disburseURL := os.Getenv(envDisburseURL)
+
+	pushName := os.Getenv(envPushUsername)
+	pushPassword := os.Getenv(envPushPassword)
+	pushBaseURL := os.Getenv(envPushBaseURL)
+	pushTokenURL := os.Getenv(envPushGetTokenURL)
+	pushPayURL := os.Getenv(envPushPayURL)
+	pushMSISDN := os.Getenv(envPushBillerMSISDN)
+	pushBillerCode := os.Getenv(envPushBillerCode)
+
+	intPushMaxAmount, err := strconv.ParseInt(pushMaxAmount, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	intPushMinAmount, err := strconv.ParseInt(pushMinAmount, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	intDisburseMaxAmount, err := strconv.ParseInt(disburseMaxAmount, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	intDisburseMinAmount, err := strconv.ParseInt(disburseMinAmount, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	return &Config{
+		ReferenceIDPrefix:         "TGCLI",
+		AppName:                   "TG",
+		AppVersion:                "v1.0.0",
+		ReleaseDate:               "JULY 2021",
+		MaxPushAmount:             intPushMaxAmount,
+		MinPushAmount:             intPushMinAmount,
+		MaxDisburseAmount:         intDisburseMaxAmount,
+		MinDisburseAmount:         intDisburseMinAmount,
+		DisburseAccountName:       disburseName,
+		DisburseAccountMSISDN:     disburseMSISDN,
+		DisburseBrandID:           disburseBrandID,
+		DisbursePIN:               disbursePIN,
+		DisburseRequestURL:        disburseURL,
+		PushUsername:              pushName,
+		PushPassword:              pushPassword,
+		PushPasswordGrantType:     "password",
+		PushApiBaseURL:            pushBaseURL,
+		PushGetTokenURL:           pushTokenURL,
+		PushBillerMSISDN:          pushMSISDN,
+		PushBillerCode:            pushBillerCode,
+		PushPayURL:                pushPayURL,
+		PushReverseTransactionURL: "nil",
+		PushHealthCheckURL:        "nil",
+	}, nil
+}
 
 func (client *Client) MakeConfigCommand() *cli.Command {
 
